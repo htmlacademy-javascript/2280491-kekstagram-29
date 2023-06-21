d497782e46213c334ecb0f13b9be54d98601db09
Nadezhda <oleneva96@gmail.com>
2023-06-21 14:30:14 +0300

    added objects for numbers in functions

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const MESSAGES = [
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Лица у людей на фотке перекошены, как будто их избивают.',
+  'Как можно было поймать такой неудачный момент?!'
 ];
 
 const NAMES = [
@@ -24,6 +25,25 @@ const DESCRIPTIONS = [
   'Не хватило слов для описания этого фото'
 ];
 
+const PHOTOS_NUMBER = {
+  max: 25,
+  min: 1,
+};
+
+const LIKES_NUMBER = {
+  min: 15,
+  max: 200,
+};
+
+const COMMENTS_NUMBER = {
+  min: 0,
+  max: 30,
+};
+
+const AVATAR_NUMBER = {
+  min: 1,
+  max: 6,
+};
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -41,7 +61,7 @@ const getComments = (numberOfComments) => {
   for (let i = 0; i < numberOfComments; i++) {
     comments.push({
       id: commentId,
-      avatar: `img/avatar-${getRandomInteger(0, 6)}.svg`,
+      avatar: `img/avatar-${getRandomInteger(AVATAR_NUMBER.min, AVATAR_NUMBER.max)}.svg`,
       message: getRandomArrayElement(MESSAGES),
       name: getRandomArrayElement(NAMES)
     });
@@ -52,13 +72,13 @@ const getComments = (numberOfComments) => {
 
 const createPhotos = () => {
   const photos = [];
-  for (let i = 1; i < 26; i++) {
+  for (let i = PHOTOS_NUMBER.min; i <= PHOTOS_NUMBER.max; i++) {
     photos.push({
       id: i,
       url: `photos/${i}.jpg`,
       description: getRandomArrayElement(DESCRIPTIONS),
-      likes: getRandomInteger(15, 200),
-      comments: getComments(getRandomInteger(0, 30))
+      likes: getRandomInteger(LIKES_NUMBER.min, LIKES_NUMBER.max),
+      comments: getComments(getRandomInteger(COMMENTS_NUMBER.min, COMMENTS_NUMBER.max))
     });
   }
   return photos;
